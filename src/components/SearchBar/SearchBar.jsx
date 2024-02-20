603b5fc495c7539cc597554461151e8729f823d7
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-02-20 01:12:02 -0700

    upd

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,21 +1,38 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useId } from "react";
+import * as Yup from "yup";
+import css from "./SearchBar.module.css";
 
 export default function SearchBar() {
-  const id = useId();
+  const FeedbackSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long")
+      .required("Required"),
+    email: Yup.string().email("Must be a valid email!").required("Required"),
+    message: Yup.string()
+      .min(3, "Too short")
+      .max(256, "Too long")
+      .required("Required"),
+    level: Yup.string().oneOf(["good", "neutral", "bad"]).required("Required"),
+  });
+
+  const nameFieldId = useId();
+  const emailFieldId = useId();
+  const textId = useId();
+  const selectId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
   return (
-    // <>
-    //   <label htmlFor={id} style={{display: "block"}}>Find contacts by name</label>
-    //   <input id={id} type="text" style={{width: '97%'}}></input>
-    // </>
-    <Formik
-      initialValues={{ username: "Mango", email: "mango@mail.com" }}
-      onSubmit={() => {}}
-    >
+    <Formik>
       <Form>
-        <Field type="text" name="username" />
-        <Field type="email" name="email" />
-        <button type="submit">Submit</button>
+        <label>Find contacts by name</label>
+        <br />
+        <Field />
       </Form>
     </Formik>
   );
