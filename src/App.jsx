1e75ff3a492bbab04b691bec8062e47690325ee5
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-02-25 22:10:44 -0700

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
-import SearchBox from "./components/SearchBar/SearchBox";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 import contacts from "./contacts.json";
 
